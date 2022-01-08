93e68c0a5d395c8449b41102b24c955dc4f92745
MohamedAboubasha <mohamedaboubasha@gmail.com>
2022-01-09 00:26:09 +0200

    Django models task

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 from profiles_api import models
 
 admin.site.register(models.UserProfile)
+admin.site.register(models.Category)
+admin.site.register(models.Course)
+admin.site.register(models.Image)
+admin.site.register(models.Video)
+admin.site.register(models.Review)
+admin.site.register(models.Instructor)
--- profiles_api/models.py
+++ profiles_api/models.py
@@ -52,3 +52,71 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         """Return string representation of our user"""
         return self.email
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        """Return string representation of category"""
+        return self.name
+
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=1023)
+    overview = models.CharField(max_length=1023)
+    price = models.IntegerField()
+    created_at = models.DateField(auto_now=False, auto_now_add=True)
+    updated_at = models.DateField(auto_now=True, auto_now_add=False)
+    language = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        """Return string representation of a course"""
+        return self.name
+
+class Image(models.Model):
+    IMAGE_CHOICES = [
+        ('cover', 'cover'),
+        ('overview', 'overview')
+    ]
+    name = models.CharField(max_length=255, choices=IMAGE_CHOICES)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    image_path = models.CharField(max_length=255)
+    # images_path = models.FileField(upload_to='uploads/'+course.__str__()+'/images/')
+
+    def __str__(self):
+        """Return string representation of course image"""
+        return self.name + " image for " + self.course.__str__()
+
+class Video(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=1023)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    video_path = models.CharField(max_length=255)
+    # video_path = models.FileField(upload_to='uploads/'+course.__str__()+'/videos/')
+
+    def __str__(self):
+        """Return string representation of course video"""
+        return self.name + " video for " + self.course.__str__()
+
+class Review(models.Model):
+    review = models.CharField(max_length=511)
+    rating = models.IntegerField()
+    created_at = models.DateField(auto_now=False, auto_now_add=True)
+    user = models.ForeignKey(UserProfile, null=True, on_delete=models.SET_NULL)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        """Return string representation of review"""
+        return self.user.get_full_name() + "'s review of " + self.course.__str__()
+
+class Instructor(models.Model):
+    name = models.CharField(max_length=255)
+    image_path = models.CharField(max_length=255)
+    description = models.CharField(max_length=1023)
+    rating = models.IntegerField()
+    courses = models.ManyToManyField(Course)
+
+    def __str__(self):
+        """Return string representation of instructor"""
+        return self.name
